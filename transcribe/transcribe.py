a13456cd8afd342520b02410e0f933545eeb7b07
Matthew Lack <matt.lack@mail.utoronto.ca>
2025-03-22 16:04:23 -0400

    Transcribing support

---
+++ transcribe/transcribe.py
@@ -0,0 +1,90 @@
+from time import sleep
+
+import cv2
+import asyncio
+
+import numpy as np
+from google import genai
+from google.genai.types import Part
+from pydantic import BaseModel
+import os
+
+class Response(BaseModel):
+    problem: str
+    position: str
+    added_text: str
+
+client = genai.Client(api_key=os.getenv("GENAI_API_KEY"))
+
+def clear_console():
+    if os.getenv('TERM'):
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:
+        print("\n" * 100)
+
+def capture_frame() -> np.ndarray:
+    cap = cv2.VideoCapture(0)
+    ret, f = cap.read()
+    cap.release()
+    # Encode to png
+    _, f = cv2.imencode(".png", f)
+    return f
+
+
+def transcribe(interval: int):
+    last_image = None
+
+    sections = []
+    current_section = ""
+
+    while True:
+        frame = capture_frame()
+        image = Part.from_bytes(data=frame.tobytes(), mime_type="image/png")
+        content = filter(lambda x: x is not None, [
+            "Look at these two images, output what is new in the second image compared to the first image."
+            "If the image is not of a board or lecture with text, please output 'INVALID' in the problem field."
+            "If the board has been cleared or the slide changed, please output 'CLEARED' in the problem field."
+            "Output only those words, or exactly what has been added between the two pictures."
+            "If there is no previous picture, output everything in the only picture available."
+            "If there are no changes. Output 'NO CHANGES' in the problem field."
+            "If there is an obstruction, output 'OBSTRUCTED' in the problem field."
+            "If there is no problem put '' in the problem field."
+            "If there is no problem I would like you to also tell me where the new text is."
+            "For the position you should output either: 'END', or 'BEFORE <text> or 'AFTER <text>'"
+            "Output '' for anything not applicable."
+            f"The current content of the board is believed to be as follows: '{current_section}'"
+            f"If this is incorrect, in the position field put 'REPLACE' and write all the text."
+            f"If Math is present, write it in latex.",
+            last_image, image])
+        raw = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=content,
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": Response
+            }
+        )
+        response = raw.parsed
+
+        if response.problem == '':
+            where = response.position
+            if where == 'END':
+                current_section += response.added_text
+            elif where.startswith('BEFORE'):
+                current_section = current_section.replace(where[7:], f' {response.added_text} ' + where[7:])
+            elif where.startswith('AFTER'):
+                current_section = current_section.replace(where[6:], where[6:] + f' {response.added_text} ')
+            elif where == 'REPLACE':
+                current_section = response.added_text
+
+        clear_console()
+        print(current_section)
+        if response.problem != 'OBSTRUCTED':
+            last_image = image
+        if response.problem == 'CLEARED':
+            sections.append(current_section)
+            current_section = ""
+        sleep(interval)
+
+
+transcribe(2)
